2b1265046d084eacb1b3acc9e4eb9c77b989314c
AlinaPredescu <alipredescu@yahoo.com>
2017-09-22 22:46:48 +0300

    tema dom

---
+++ curs 6 - 9sept DOM/tema2 X0/script.js
@@ -0,0 +1,11 @@
+function schimbaImagineaX(event,elem){
+    
+        elem.src="x12.jpg"
+    
+    }
+
+    function schimbaImagineaBlank(event,elem){
+        
+            elem.src="blank.jpg"
+        
+        }
