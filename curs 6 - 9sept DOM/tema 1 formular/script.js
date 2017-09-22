860cba96c1a6994293705978c79b876d9825e0aa
AlinaPredescu <alipredescu@yahoo.com>
2017-09-22 23:49:44 +0300

    dom1

---
+++ curs 6 - 9sept DOM/tema 1 formular/script.js
@@ -0,0 +1,8 @@
+function comparareInput() {
+    var firstName =  getElementById("input1");
+    var oras = getElementById("input2");
+    if (firstName === oras) {  document.getElementById("personale").style.color = "green"
+}
+{return false;
+}
+};
