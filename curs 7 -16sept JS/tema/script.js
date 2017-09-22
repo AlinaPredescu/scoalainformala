b407ce758cb67f972c9eeb05438272993e956e07
AlinaPredescu <alipredescu@yahoo.com>
2017-09-22 22:09:34 +0300

    tema curs 7

---
+++ curs 7 -16sept JS/tema/script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(1);
+    var cell1 = row.insertCell(0);
+     cell1.innerHTML = document.getElementById("inserari");
+}
