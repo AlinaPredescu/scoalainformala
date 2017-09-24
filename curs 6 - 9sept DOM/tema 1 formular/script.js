ff394d0935cc33e0837f6077f1401adf80e6e182
AlinaPredescu <alipredescu@yahoo.com>
2017-09-24 20:11:20 +0300

    modif teme dom

--- curs 6 - 9sept DOM/tema 1 formular/script.js
+++ curs 6 - 9sept DOM/tema 1 formular/script.js
@@ -1,8 +1,6 @@
 function comparareInput() {
     var firstName =  getElementById("input1");
     var oras = getElementById("input2");
-    if (firstName === oras) {  document.getElementById("personale").style.color = "green"
-}
-{return false;
-}
+    if (firstName === oras) {  document.getElementById("personale").style.color = "green";
+} 
 };
--- curs 7 -16sept JS/tema/script.js
+++ curs 7 -16sept JS/tema/script.js
@@ -2,5 +2,5 @@ function myFunction() {
     var table = document.getElementById("myTable");
     var row = table.insertRow(1);
     var cell1 = row.insertCell(0);
-     cell1.innerHTML = document.getElementById("inserari");
+     cell1.innerHTML = document.getElementById("inserari").value;
 }
