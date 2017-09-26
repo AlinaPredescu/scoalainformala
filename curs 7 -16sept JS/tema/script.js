b21cd88a400e22368e5002f16764ed130710f85e
AlinaPredescu <alipredescu@yahoo.com>
2017-09-26 07:18:13 +0300

    twma 25.09

--- curs 7 -16sept JS/tema/script.js
+++ curs 7 -16sept JS/tema/script.js
@@ -1,6 +1,18 @@
+
+var elevi=[];
+
 function myFunction() {
     var table = document.getElementById("myTable");
     var row = table.insertRow(1);
     var cell1 = row.insertCell(0);
      cell1.innerHTML = document.getElementById("inserari").value;
+     elevi.push(cell1);
+}
+
+function creareButon(){
+
+    var cell3 = document.createElement("BUTTON");
+    var numeBtn=document.createTextNode("Vezi note");
+    cell3.appendChild(numeBtn);
+    document.body.appendChild(cell3);
 }
