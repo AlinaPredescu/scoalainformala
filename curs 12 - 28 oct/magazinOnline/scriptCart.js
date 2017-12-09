026bf56ab6fce1cd87436dba35c1ab21ac0993ca
AlinaPredescu <alipredescu@yahoo.com>
2017-12-09 13:51:13 +0200

    10.12.2017

--- curs 12 - 28 oct/magazinOnline/scriptCart.js
+++ curs 12 - 28 oct/magazinOnline/scriptCart.js
@@ -49,15 +49,16 @@ function drawCos(lista_produse){
 
     <table id="myTable">
     <tr>
-        <td width=30%>Nume</td>
-        <td width=5%>Pret</td>
-       
-        <td width=10%>Cantitate</td>
-       
-        <td width=10%>Sub total</td>
+        <td width=30%><b>Nume</b></td>
+        <td width=5%><b>Pret</b></td>
+        <td width=10%><b>Cantitate</B></td>
+        <td width=10%><B>Subtotal</b></td>
         <td width=10%></td>
     </tr>
+
     `;
+
+var total=0;
 
 for (var i in lista_produse){
         html= html + `
@@ -65,14 +66,19 @@ for (var i in lista_produse){
             <td>${lista_produse[i].nume}</td>
             <td>${lista_produse[i].pret}</td>
           
-            <td><span class="butonCantitate" onclick="scadeCantitate('${i}', ${lista_produse[i].cantitate}, ${lista_produse[i].idProdus})"> - </span> ${lista_produse[i].cantitate} <span class="butonCantitate" onclick="cresteCantitate('${i}', ${lista_produse[i].cantitate}, )"> + </span></td>
+            <td><span class="butonCantitate" onclick="scadeCantitate('${i}', ${lista_produse[i].cantitate}, ${lista_produse[i].idProdus})"> - </span>
+            ${lista_produse[i].cantitate} 
+            <span class="butonCantitate" onclick="cresteCantitate('${i}', ${lista_produse[i].cantitate}, ${lista_produse[i].idProdus})"> + </span></td>
        
-            <td>${lista_produse[i].subTotal}</td>
+            <td>${lista_produse[i].cantitate*lista_produse[i].pret}</td>
             <td><button onclick="stergeProdus('${i}')"> Sterge </button></td>
                     </tr>`
 
-
+total+=lista_produse[i].cantitate*lista_produse[i].pret
     }
+    document.getElementById("sumaSubtotal").innerHTML=(total/1.19).toFixed(2);
+    document.getElementById("tva").innerHTML=(-total/1.19+total).toFixed(2);
+    document.getElementById("total").innerHTML=total+0;
     html = html + `
         </table>
     `;
@@ -88,35 +94,48 @@ function stergeProdus(produsDeSters){
  }
 
 
- function cresteCantitate(idCantitate, cantitate, idProdus) {
+
+function cresteCantitate(idCantitate, cantitate, idProdus) {
     cantitate++;
-    if(cantitate> makeGetFirebase("https://magazinonlinealina1.firebaseio.com/produse/"+idProdus+".json")){
-    makePutFirebase("https://magazinonlinealina1.firebaseio.com/shoppingCart/"+idCantitate+"/cantitate.json",cantitate);}
-    else {
-        alert("Stoc epuizat!");
-    }
+    verificaStoc("https://magazinonlinealina1.firebaseio.com/produse/"+idProdus+"/stoc.json",idCantitate,cantitate)
+}
+function verificaStoc(url,idCantitate,cantitate){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // Typical action to be performed when the document is ready:
+            if(cantitate<= xhttp.responseText){
+                makePutFirebase("https://magazinonlinealina1.firebaseio.com/shoppingCart/"+idCantitate+"/cantitate.json",cantitate);}
+                else {
+                    alert("Stoc epuizat!");
+                }
+
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send(); 
 }
 
 
- 
-  /*function cresteCantitate(idCantitate, cantitate) {
-    cantitate++;
-    makePutFirebase("https://magazinonlinealina1.firebaseio.com/shoppingCart/"+idCantitate+"/cantitate.json",cantitate)
-    
- }*/
-
  function scadeCantitate(idCantitate, cantitate) {
     cantitate--;
-    makePutFirebase("https://magazinonlinealina1.firebaseio.com/shoppingCart/"+idCantitate+"/cantitate.json",cantitate)
-    
- }
-
-
- /*function scadeCantitate(idCantitate, cantitate) {
-    cantitate--;
-    if(cantitate<=0 ){
+    if(cantitate>=0 ){
         makePutFirebase("https://magazinonlinealina1.firebaseio.com/shoppingCart/"+idCantitate+"/cantitate.json",cantitate);}
         else {
             alert("Cantitatea nu poate fi negativa!");
         }
-    }*/
+    }
+
+    function updateCos(lista_produse){
+        var s=0;
+        for (var i in lista_produse){
+            s+=lista_produse[i].cantitate;
+        
+        }
+        document.getElementById("buton2").innerHTML=s + " produse in cos";
+    }
+
+    function sumaSubtotal(){
+
+    }
+
