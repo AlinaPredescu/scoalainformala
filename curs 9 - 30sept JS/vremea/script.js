15c1ee059042595b9389155133af3b372a2595cd
AlinaPredescu <alipredescu@yahoo.com>
2018-01-03 00:01:19 +0200

    2.01.2018

--- curs 9 - 30sept JS/vremea/script.js
+++ curs 9 - 30sept JS/vremea/script.js
@@ -73,34 +73,32 @@ function afiseazaPrognoza() {
             console.log(json)
             gJson = json;
 
+            var prognoze = JSON.parse(json).list;
+            var trs={};
+            for (var i = 0; i < prognoze.length; i++) {
+                var prognoza = prognoze[i];
+                prognoza.zi = getData(prognoza.dt_txt);
+                if (trs[prognoza.zi]===undefined){
+                    trs[prognoza.zi]=1;
+                }else{
+                    trs[prognoza.zi]++;
+                }
+                prognoza.ora = getOra(prognoza.dt_txt);
+                prognoza.descriere = prognoza.weather[0].description;
+                prognoza.temperatura = prognoza.main.temp;
+                prognoza.imagine = URL_WEATHER_ICON_PREFIX + prognoza.weather[0].icon + ".png";
 
-            var ziua = JSON.parse(json).list[4].dt_txt;
-            document.getElementById("ziua").innerHTML = getData(ziua);
-
-            var ora = JSON.parse(json).list[4].dt_txt;
-            document.getElementById("ora").innerHTML = getOra(ora);
-
-            var descriere = JSON.parse(json).list[4].weather[0].description;
-            document.getElementById("descriere1").innerHTML = descriere;
-
-            var temperatura = JSON.parse(json).list[4].main.temp;
-            document.getElementById("temperatura").innerHTML = temperatura;
-
-            var imagine = URL_WEATHER_ICON_PREFIX + JSON.parse(json).list[4].weather[0].icon + ".png";
-            document.getElementById("imagine1").src = imagine;
-
-            var imagine7 = URL_WEATHER_ICON_PREFIX + JSON.parse(json).list[6].weather[0].icon + ".png";
-            document.getElementById("imagine7").src = imagine7;
-
-            var ora7 = JSON.parse(json).list[6].dt_txt;
-            document.getElementById("ora7").innerHTML = getOra7(ora7);
-
-            var descriere7 = JSON.parse(json).list[6].weather[0].description;
-            document.getElementById("descriere7").innerHTML = descriere7;
-
-            var temperatura7 = JSON.parse(json).list[6].main.temp;
-            document.getElementById("temperatura7").innerHTML = temperatura7;
-
+                var str = `
+                <td width=17.7%>
+                    <p>Ziua: ${prognoza.zi}</p>
+                    <img src="${prognoza.imagine}"/>
+                    <p>Ora:${prognoza.ora}</p>
+                    <p>Temperatura:${prognoza.temperatura}</p>
+                    <p>Descriere: ${prognoza.descriere} </p>
+                </td>
+                `
+                document.querySelector("#myTable tr:nth-of-type("+trs[prognoza.zi]+")").innerHTML+=str;
+            }
         }
     };
     xhttp.open("GET", URL_FORECAST_WEATHER + oras, true);
@@ -126,267 +124,3 @@ function getOra(ziua) {
     }
     return ora;
 }
-
-function getOra7(ziua7) {
-    var ora7 = "";
-    for (i = 0; i < ziua7.length; i++) {
-        if (i >= (ziua7.length - 9)) {
-            ora7 = ora7 + ziua7[i];
-        }
-    }
-    return ora7;
-}
-
-function afiseazaPrognoza2() {
-    var oras = document.getElementById("oras").value;
-    console.log(oras);
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var json = this.responseText;
-            console.log(json)
-            gJson = json;
-
-
-            var ziua2 = JSON.parse(json).list[12].dt_txt;
-            document.getElementById("ziua2").innerHTML = getData2(ziua2);
-
-            var ora2 = JSON.parse(json).list[12].dt_txt;
-            document.getElementById("ora2").innerHTML = getOra2(ora2);
-
-            var descriere = JSON.parse(json).list[12].weather[0].description;
-            document.getElementById("descriere2").innerHTML = descriere;
-
-            var temperatura = JSON.parse(json).list[12].main.temp;
-            document.getElementById("temperatura2").innerHTML = temperatura;
-
-            var imagine2 = URL_WEATHER_ICON_PREFIX + JSON.parse(json).list[12].weather[0].icon + ".png";
-            document.getElementById("imagine2").src = imagine2;
-
-        }
-    };
-    xhttp.open("GET", URL_FORECAST_WEATHER + oras, true);
-    xhttp.send();
-
-}
-
-function getData2(ziua2) {
-    var data = "";
-    for (i = 0; (i < ziua2.length - 9); i++) {
-        data = data + ziua2[i];
-    }
-    return data;
-}
-
-
-function getOra2(ziua2) {
-    var ora2 = "";
-    for (i = 0; i < ziua2.length; i++) {
-        if (i >= (ziua2.length - 9)) {
-            ora2 = ora2 + ziua2[i];
-        }
-    }
-    return ora2;
-}
-
-function afiseazaPrognoza3() {
-    var oras = document.getElementById("oras").value;
-    console.log(oras);
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var json = this.responseText;
-            console.log(json)
-            gJson = json;
-
-
-            var ziua3 = JSON.parse(json).list[20].dt_txt;
-            document.getElementById("ziua3").innerHTML = getData3(ziua3);
-
-            var ora3 = JSON.parse(json).list[20].dt_txt;
-            document.getElementById("ora3").innerHTML = getOra3(ora3);
-
-            var descriere = JSON.parse(json).list[20].weather[0].description;
-            document.getElementById("descriere3").innerHTML = descriere;
-
-            var temperatura = JSON.parse(json).list[20].main.temp;
-            document.getElementById("temperatura3").innerHTML = temperatura;
-
-            var imagine3 = URL_WEATHER_ICON_PREFIX + JSON.parse(json).list[20].weather[0].icon + ".png";
-            document.getElementById("imagine3").src = imagine3;
-
-        }
-    };
-    xhttp.open("GET", URL_FORECAST_WEATHER + oras, true);
-    xhttp.send();
-
-}
-
-function getData3(ziua3) {
-    var data = "";
-    for (i = 0; (i < ziua3.length - 9); i++) {
-        data = data + ziua3[i];
-    }
-    return data;
-}
-
-
-function getOra3(ziua3) {
-    var ora3 = "";
-    for (i = 0; i < ziua3.length; i++) {
-        if (i >= (ziua3.length - 9)) {
-            ora3 = ora3 + ziua3[i];
-        }
-    }
-    return ora3;
-}
-
-function afiseazaPrognoza4() {
-    var oras = document.getElementById("oras").value;
-    console.log(oras);
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var json = this.responseText;
-            console.log(json)
-            gJson = json;
-
-
-            var ziua4 = JSON.parse(json).list[28].dt_txt;
-            document.getElementById("ziua4").innerHTML = getData4(ziua4);
-
-            var ora4 = JSON.parse(json).list[28].dt_txt;
-            document.getElementById("ora4").innerHTML = getOra4(ora4);
-
-            var descriere = JSON.parse(json).list[28].weather[0].description;
-            document.getElementById("descriere4").innerHTML = descriere;
-
-            var temperatura = JSON.parse(json).list[28].main.temp;
-            document.getElementById("temperatura4").innerHTML = temperatura;
-
-            var imagine4 = URL_WEATHER_ICON_PREFIX + JSON.parse(json).list[28].weather[0].icon + ".png";
-            document.getElementById("imagine4").src = imagine4;
-
-        }
-    };
-    xhttp.open("GET", URL_FORECAST_WEATHER + oras, true);
-    xhttp.send();
-
-}
-
-function getData4(ziua4) {
-    var data = "";
-    for (i = 0; (i < ziua4.length - 9); i++) {
-        data = data + ziua4[i];
-    }
-    return data;
-}
-
-function getOra4(ziua4) {
-    var ora4 = "";
-    for (i = 0; i < ziua4.length; i++) {
-        if (i >= (ziua4.length - 9)) {
-            ora4 = ora4 + ziua4[i];
-        }
-    }
-    return ora4;
-}
-
-
-function afiseazaPrognoza5() {
-    var oras = document.getElementById("oras").value;
-    console.log(oras);
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var json = this.responseText;
-            console.log(json)
-            gJson = json;
-
-
-            var ziua5 = JSON.parse(json).list[36].dt_txt;
-            document.getElementById("ziua5").innerHTML = getData5(ziua5);
-            var ora5 = JSON.parse(json).list[36].dt_txt;
-            document.getElementById("ora5").innerHTML = getOra5(ora5);
-
-            var descriere = JSON.parse(json).list[36].weather[0].description;
-            document.getElementById("descriere5").innerHTML = descriere;
-
-            var temperatura = JSON.parse(json).list[36].main.temp;
-            document.getElementById("temperatura5").innerHTML = temperatura;
-
-            var imagine5 = URL_WEATHER_ICON_PREFIX + JSON.parse(json).list[36].weather[0].icon + ".png";
-            document.getElementById("imagine5").src = imagine5;
-
-
-        }
-    };
-    xhttp.open("GET", URL_FORECAST_WEATHER + oras, true);
-    xhttp.send();
-
-}
-
-function getData5(ziua5) {
-    var data = "";
-    for (i = 0; (i < ziua5.length - 9); i++) {
-        data = data + ziua5[i];
-    }
-    return data;
-}
-
-function getOra5(ziua5) {
-    var ora5 = "";
-    for (i = 0; i < ziua5.length; i++) {
-        if (i >= (ziua5.length - 9)) {
-            ora5 = ora5 + ziua5[i];
-        }
-    }
-    return ora5;
-}
-
-function afiseazaPrognoza6() {
-    var oras = document.getElementById("oras").value;
-    console.log(oras);
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var json = this.responseText;
-            console.log(json)
-            gJson = json;
-
-            var ziua6 = JSON.parse(json).list[0].dt_txt;
-            document.getElementById("ziua6").innerHTML = getData6(ziua6);
-            var ora6 = JSON.parse(json).list[0].dt_txt;
-            document.getElementById("ora6").innerHTML = getOra6(ora6);
-            var descriere = JSON.parse(json).list[0].weather[0].description;
-            document.getElementById("descriere6").innerHTML = descriere;
-            var temperatura = JSON.parse(json).list[0].main.temp;
-            document.getElementById("temperatura6").innerHTML = temperatura;
-            var imagine6 = URL_WEATHER_ICON_PREFIX + JSON.parse(json).list[0].weather[0].icon + ".png";
-            document.getElementById("imagine6").src = imagine6;
-
-        }
-    };
-    xhttp.open("GET", URL_FORECAST_WEATHER + oras, true);
-    xhttp.send();
-
-}
-
-function getData6(ziua6) {
-    var data = "";
-    for (i = 0; (i < ziua6.length - 9); i++) {
-        data = data + ziua6[i];
-    }
-    return data;
-}
-function getOra6(ziua6) {
-    var ora6 = "";
-    for (i = 0; i < ziua6.length; i++) {
-        if (i >= (ziua6.length - 9)) {
-            ora6 = ora6 + ziua6[i];
-        }
-    }
-    return ora6;
-}
-
-
